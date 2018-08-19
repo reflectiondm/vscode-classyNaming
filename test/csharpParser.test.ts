08e9d12da830e38c36b3fc091ca1b30f2e6c9325
Orion Kindel <cakekindel@gmail.com>
2018-08-19 01:24:18 -0700

    Updated tests for multiple case support

--- test/csharpParser.test.ts
+++ test/csharpParser.test.ts
@@ -29,13 +29,27 @@ suite("C# parser", () => {
                 " public static TestConstructor(ISomeInterface ",
                 " public readonly TestConstructor(ISomeInterface "]
                 .forEach((line) => {
-                    test("should be able to provide suggested names", () => {
+                    test("should suggest PascalCase and camelCase for public names", () => {
                         const result = getSuggestions(line);
                         expect(result).to.eql([
-                            "_interface",
+                            "Interface",
                             "interface",
-                            "_someInterface",
+                            "SomeInterface",
                             "someInterface",
+                        ]);
+                    });
+                });
+
+            [" private static MyPoco[] ",
+                " private readonly static ICustomImplementedEnumerable<MyPoco> "]
+                .forEach((line) => {
+                    test("should suggest _underscore and camelCase for private names", () => {
+                        const result = getSuggestions(line);
+                        expect(result).to.eql([
+                            "pocos",
+                            "_pocos",
+                            "myPocos",
+                            "_myPocos",
                         ]);
                     });
                 });
@@ -61,29 +75,34 @@ suite("C# parser", () => {
                 expect(result).to.eql([]);
             });
 
-            test("should provide suggestions with user input", () => {
+            test("should provide pascal suggestions with pascal user input", () => {
+                const input = "  public ISomeType My";
+                const result = getSuggestions(input);
+                expect(result).to.eql(["MyType", "myType", "MySomeType", "mySomeType"]);
+            });
+
+            test("should provide camel suggestions with camel user input", () => {
                 const input = "  public ISomeType my";
                 const result = getSuggestions(input);
-                expect(result).to.contain("mySomeType");
+                expect(result).to.eql(["MyType", "myType", "MySomeType", "mySomeType"]);
             });
 
             test("should merge user input with name part if they are alike", () => {
                 const input = "  public ISomeComplexType som";
                 const result = getSuggestions(input);
-                expect(result).to.eql(["someType", "someComplexType"]);
+                expect(result).to.eql(["SomeType", "someType", "SomeComplexType", "someComplexType"]);
             });
 
             test("should not provide duplicate suggestions", () => {
                 const input = "  public ISomeType some";
                 const result = getSuggestions(input);
-                expect(result).to.eql(["someType"]);
+                expect(result).to.eql(["SomeType", "someType"]);
             });
 
             test("should provide suggestions with lowercase character after  _ ", () => {
                 const input = "  public ISomeType _";
                 const result = getSuggestions(input);
-                expect(result).to.contain("_someType");
-                expect(result).to.contain("_type");
+                expect(result).to.eql(["type", "_type", "someType", "_someType"]);
             });
 
             ["ICollection", "ObservableCollection", "DbSet", "List", "IEnumerable", "IList", "LinkedList"]
@@ -91,8 +110,7 @@ suite("C# parser", () => {
                     test("should pluralize suggested name for collections like " + typeName, () => {
                         const input = "   public " + typeName + "<" + data.WellKnownInterface + "> ";
                         const result = getSuggestions(input);
-                        expect(result).to.contain("someInterfaces");
-                        expect(result).to.contain("interfaces");
+                        expect(result).to.eql(["Interfaces", "interfaces", "SomeInterfaces", "someInterfaces"]);
                     });
                 });
 
@@ -123,7 +141,7 @@ suite("C# parser", () => {
             expect(result).to.equal(data.WellKnownInterface);
         });
 
-        test("should igore multiple keywords", () => {
+        test("should ignore multiple keywords", () => {
             const input = " private const ISomeInterface ";
             const result = target.getParsingResult(input).typeName;
             expect(result).to.equal("ISomeInterface");
