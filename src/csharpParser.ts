33bcd0776427977258d7e62f96a802a3f5b3f600
Orion Kindel <cakekindel@gmail.com>
2018-08-19 01:23:22 -0700

    refactor combineSuggestions()
    Replaced by generateSuggestions and renamed to getPascalSuggestions.
    
    generateSuggestions() now handles the case-by-case logic
    and user input

--- src/csharpParser.ts
+++ src/csharpParser.ts
@@ -1,5 +1,6 @@
 import DeclarationInfo from "./declarationInfo";
-import * as pluralizer from "pluralize";
+import { plural as pluralize } from "pluralize";
+import { stringBeginsWith, arrayIncludesAny } from './utils';
 
 export class CsharpParser {
     public splitTypeName(typeName: string): string[] {
@@ -18,22 +19,17 @@ export class CsharpParser {
         return result.map((m) => m.toLowerCase());
     }
 
-    public combineSuggestions(parts: string[], config: { includePascal?: boolean, includeUnderscore?: boolean }): string[] {
+    public getPascalSuggestions(parts: string[]): string[] {
         const result = [];
         for (let i = parts.length - 1; i >= 0; i--) {
             let suggestion = "";
             for (let j = i; j < parts.length; j++) {
-                suggestion += this.ToPascal(parts[j]);
+                suggestion += this.toCase("pascal", parts[j]);
             }
 
-            if (config.includePascal) {
-                result.push(suggestion);
-            }
-            if (config.includeUnderscore) {
-                result.push("_" + this.ToCamel(suggestion));
-            }
-            result.push(this.ToCamel(suggestion));
+            result.push(suggestion);
         }
+
         return result;
     }
 
@@ -42,24 +38,14 @@ export class CsharpParser {
             return [];
         }
 
-        const isPrivate = declarationInfo.getIsPrivate();
-        const typeName = declarationInfo.getIsPlural() ?
-            pluralizer.plural(declarationInfo.getTypeName()) :
-            declarationInfo.getTypeName();
-
-        const nameParts = this.splitTypeName(typeName);
-        const result = new Set();
         const userInput = declarationInfo.getUserInput();
-        const suggestions = this.combineSuggestions(nameParts, { includePascal: !isPrivate, includeUnderscore: isPrivate && !userInput });
-        const prefix = nameParts.find((part) => part.includes(userInput)) || userInput;
-        if (userInput !== "") {
-            suggestions.map((s) => s.includes(prefix, 0) ? s : this.combineWithUserInput(s, prefix))
-                .forEach((s) => result.add(s));
-        } else {
-            suggestions.forEach((s) => result.add(s));
-        }
+        const isPrivate = declarationInfo.getIsPrivate();
 
-        return [...result];
+        const typeName = declarationInfo.getIsPlural() ? pluralize(declarationInfo.getTypeName()) : declarationInfo.getTypeName();
+        const splitTypeName = this.splitTypeName(typeName);
+
+        const allowedCases: IAllowedCases = { camel: true, _camel: isPrivate && !userInput, pascal: !isPrivate };
+        return this.generateSuggestions(userInput, splitTypeName, allowedCases);
     }
 
     public getParsingResult(input: string): IParsingResult {
@@ -70,19 +56,94 @@ export class CsharpParser {
         return { suggestions, typeName, userInput };
     }
 
-    private ToPascal(input: string): string {
-        return input[0].toUpperCase() + input.substring(1);
+    private generateSuggestions(userInput: string, splitTypeName: string[], allowedCases: IAllowedCases): string[] {
+        let prefix = "";
+        if (userInput) {
+            prefix = splitTypeName.find((word) => word.toLowerCase().includes(userInput.toLowerCase())) || userInput;
+        }
+
+        const result: string[] = [];
+
+        const suggestions = this.getPascalSuggestions(splitTypeName);
+        for (let Suggestion of suggestions) {
+            // tslint:disable-next-line:variable-name
+            let _suggestion = this.toCase("_camel", Suggestion);
+            let suggestion = this.toCase("camel", Suggestion);
+
+            if (!stringBeginsWith(Suggestion, prefix, { caseSensitive: false })) {
+                _suggestion = this.toCase("_camel", prefix + Suggestion);
+                suggestion = this.toCase("camel", prefix + Suggestion);
+                Suggestion = this.toCase("pascal", prefix + Suggestion);
+            }
+
+            if (arrayIncludesAny(result, Suggestion, suggestion, _suggestion)) {
+                break;
+            }
+
+            if (this.caseOf(userInput) === "_camel") {
+                allowedCases.pascal = false;
+                allowedCases._camel = true;
+            } else if (this.caseOf(userInput) === "pascal") {
+                allowedCases.pascal = true;
+                allowedCases._camel = false;
+            }
+
+            if (allowedCases.pascal) {
+                result.push(Suggestion);
+            }
+            if (allowedCases.camel) {
+                result.push(suggestion);
+            }
+            if (allowedCases._camel) {
+                result.push(_suggestion);
+            }
+        }
+
+        return result;
     }
 
-    private ToCamel(input: string): string {
-        return input[0].toLowerCase() + input.substring(1);
+    private toCase(toType: 'pascal' | 'camel' | '_camel', str: string): string {
+        if (str === "_" && toType === "_camel") {
+            return "_";
+        } else if (str === "_" && toType !== "_camel") {
+            return "";
+        }
+
+        if (str[0] === "_") {
+            str = str.substr(1);
+        }
+
+        if (toType === "camel") {
+            str = str[0].toLowerCase() + str.substr(1);
+        }
+        if (toType === "_camel") {
+            str = "_" + str[0].toLowerCase() + str.substr(1);
+        }
+        if (toType === "pascal") {
+            str = str[0].toUpperCase() + str.substr(1);
+        }
+
+        return str;
     }
 
-    private combineWithUserInput(suggestion: string, userInput: string): string {
-        return userInput === "_" ?
-            userInput + suggestion :
-            userInput + this.ToPascal(suggestion);
+    private caseOf(str: string): 'pascal' | 'camel' | '_camel' {
+        const firstChar = str[0];
+        const uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        if (firstChar === "_") {
+            return "_camel";
+        } else if (uppercaseAlphabet.includes(firstChar)) {
+            return "pascal";
+        } else {
+            return "camel";
+        }
     }
+}
+
+interface IAllowedCases {
+    pascal: boolean;
+    _camel: boolean;
+    camel: boolean;
 }
 
 export interface IParsingResult {
