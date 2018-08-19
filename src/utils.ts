77d3e9a5c2ef0b2dc42629ed7953b0a16a37fb3a
Orion Kindel <cakekindel@gmail.com>
2018-08-19 01:23:50 -0700

    Add arrayIncludesAny() and stringBeginsWith() helpers

--- src/utils.ts
+++ src/utils.ts
@@ -8,6 +8,36 @@ function getCharacters(from: string, to: string): string[] {
     return result;
 }
 
+/** Determines if a string begins with a substring */
+function stringBeginsWith(str: string, substr: string, options = { caseSensitive: true }): boolean {
+    if (substr.length > str.length) {
+        return false;
+    }
+
+    if (!substr || substr.length === 0) {
+        return true;
+    }
+
+    if (!options.caseSensitive) {
+        str = str.toLowerCase();
+        substr = substr.toLowerCase();
+    }
+
+    str = str.substr(0, substr.length);
+    return str === substr;
+}
+
+/** Determines whether an array contains any of a list of values */
+function arrayIncludesAny<T>(array: T[], ...values: T[]): boolean {
+    for (const val of values) {
+        if (array.indexOf(val) > -1) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 function concat(...args) {
     return args.reduce((concatenated: [any], current: [any]) => {
         return concatenated.concat(current);
@@ -16,7 +46,9 @@ function concat(...args) {
 
 export default {
     getCharacters,
+    stringBeginsWith,
+    arrayIncludesAny,
     concat,
 };
 
-export { getCharacters, concat };
+export { getCharacters, stringBeginsWith, arrayIncludesAny, concat };
